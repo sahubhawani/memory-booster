d045f73b4a9ee145a22481855813e7a379cae96e
BhawaniShankar <bhawanishankar321@gmail.com>
2021-09-23 15:19:06 +0530

    New Feature: Auto remind added

--- bot.py
+++ bot.py
@@ -1,21 +1,26 @@
-from telegram import Update
-from telegram.ext import Updater, inlinequeryhandler, CommandHandler, MessageHandler, Filters
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
 import psycopg2
 import os
-from datetime import date, timedelta, datetime
+from datetime import date, timedelta, datetime, time
 
+
+# Heroku cloud server parameter
 TOKEN = os.environ.get('TOKEN')
 PORT = int(os.environ.get('PORT', 5000))
 
-# Database Parameters
+# Heroku Database Parameters
 heroku_host = os.environ.get('HOST')
 heroku_database = os.environ.get('DATABASE')
 heroku_user = os.environ.get('USER')
 heroku_password = os.environ.get('PASSWORD')
 heroku_port = int(os.environ.get('DATABASE_PORT'))
+
+
+TOKEN = "1424695833:AAHlnvfy57di4GL7BvB8VcUF89AN0rU2PCw"
+
 # Staring the Database
 conn = psycopg2.connect(dbname=heroku_database, user=heroku_user, password=heroku_password, host=heroku_host,
-                        port=heroku_port)
+                        )  # port=heroku_port
 
 remind_period0 = timedelta(days=0)
 remind_period3 = timedelta(days=3)
@@ -30,7 +35,7 @@ def delete_from_db(topic_date_str):
             cur.execute("DELETE FROM topiclist WHERE topicdate = %s ", (topic_date,))
 
 
-def write_to_db(bot, update):  #
+def write_to_db(update, context: CallbackContext):  #
     chat_id = update.message.chat_id
     message = update.message.text  #
     try:
@@ -39,7 +44,7 @@ def write_to_db(bot, update):  #
         if first_str == "del":
             topic_date_str = message.split(",")[1].strip()
             delete_from_db(topic_date_str)
-            bot.send_message(chat_id=chat_id, text="Entries for the given date is deleted from the Database")
+            context.bot.send_message(chat_id=chat_id, text="Entries for the given date is deleted from the Database")
         else:
             topic_date = datetime.strptime(first_str, "%Y/%m/%d")
             topic = message.split(",")[1].strip()
@@ -49,11 +54,12 @@ def write_to_db(bot, update):  #
     with conn:
         with conn.cursor() as cur:
             cur.execute("INSERT INTO topiclist (topicdate,topic) VALUES(%s,%s)", (topic_date, topic))
-    bot.send_message(chat_id=chat_id, text="Thanks, this topic is added to the Database")
+    context.bot.send_message(chat_id=chat_id, text="Thanks, this topic is added to the Database")
 
-def remind(bot, update):
+
+def remind(update, context: CallbackContext):
     chat_id = update.message.chat_id
-    bot.send_message(chat_id=chat_id, text="Revise the below topics today:")
+    context.bot.send_message(chat_id=chat_id, text="Revise the below topics tomorrow:")
     with conn:
         with conn.cursor() as cur:
             cur.execute("SELECT * FROM topiclist")
@@ -63,12 +69,27 @@ def remind(bot, update):
         topic_date = data[i][0]
         if date.today() - topic_date == remind_period0 or date.today() - topic_date == remind_period3 or date.today() - topic_date == remind_period7 or date.today() - topic_date == remind_period21:
             topic = "[" + str(topic_date) + "] " + data[i][1]
-            bot.send_message(chat_id=chat_id, text=topic)
+            context.bot.send_message(chat_id=chat_id, text=topic)
 
 
-def show_all(bot, update):
+def auto_remind(context: CallbackContext):
+    chat_id = "-1001248983748"
+    context.bot.send_message(chat_id=chat_id, text="Revise the below topics tomorrow:")
+    with conn:
+        with conn.cursor() as cur:
+            cur.execute("SELECT * FROM topiclist")
+            data = cur.fetchall()
+
+    for i in range(len(data)):
+        topic_date = data[i][0]
+        if date.today() - topic_date == remind_period0 or date.today() - topic_date == remind_period3 or date.today() - topic_date == remind_period7 or date.today() - topic_date == remind_period21:
+            topic = "[" + str(topic_date) + "] " + data[i][1]
+            context.bot.send_message(chat_id=chat_id, text=topic)
+
+
+def show_all(update, context: CallbackContext):
     chat_id = update.message.chat_id
-    bot.send_message(chat_id=chat_id, text="Here is the list of all the topics in the Database:")
+    context.bot.send_message(chat_id=chat_id, text="Here is the list of all the topics in the Database:")
     with conn:
         with conn.cursor() as cur:
             cur.execute("SELECT * FROM topiclist")
@@ -77,19 +98,21 @@ def show_all(bot, update):
     for i in range(len(data)):
         topic_date = data[i][0]
         topic = "[" + str(topic_date) + "] " + data[i][1]
-        bot.send_message(chat_id=chat_id, text=topic)
+        context.bot.send_message(chat_id=chat_id, text=topic)
 
 
-def start(bot, update):
+def start(update, context: CallbackContext):
     chat_id = update.message.chat_id
     title = update.message.from_user["first_name"]
-    bot.send_message(chat_id=chat_id,
-                     text="Hi " + str(title) + ", i am glad to see you using this bot.")
+    context.bot.send_message(chat_id=chat_id,
+                             text="Hi " + str(title) + ", i am glad to see you using this bot.")
 
 
 def main():
-    updater = Updater(TOKEN)
+    updater = Updater(TOKEN, use_context=True)
+    job_queue = updater.job_queue
     dp = updater.dispatcher
+    job_queue.run_daily(auto_remind, time(hour=9, minute=35, second=00))
     dp.add_handler(MessageHandler(Filters.text & ~Filters.command, write_to_db))
     dp.add_handler(CommandHandler("remind", remind))
     dp.add_handler(CommandHandler("show_all", show_all))
@@ -103,5 +126,4 @@ def main():
 
 if __name__ == '__main__':
     main()
-# Close the Database connection
 conn.close()
